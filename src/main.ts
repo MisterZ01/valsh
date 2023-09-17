199dfe43bae6330039c24ffec2fabff48041ae9d
COULIBALY Zoumana <coulibalyzoumana4881@gmail.com>
2023-09-17 08:45:53 +0000

    ajout de port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.PORT ||3000);
 }
 // regarde si tu vois la route auth dedans 
 bootstrap();
